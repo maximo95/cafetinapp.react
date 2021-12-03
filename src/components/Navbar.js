4be736c79b30059ab9b5e593077cb11c0fd4b481
maximo95 <maximollallire@gmail.com>
2021-12-03 16:01:56 -0300

    imagen logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import './Navbar.css'
-
+import logo_para_html_cafe from  '../assets/logo_para_html_cafe.png'
 function Navbar () {
     return (
         <nav>
+            <a  href= "#"> <img scr ={logo_para_html_cafe} className="imagenLogo" /> </a>
             <a  href= "#" className="nav-clase">home</a>
             <a  href= "#" className="nav-clase">productos</a>
             <a  href= "#" className="nav-clase">nosotros</a>
