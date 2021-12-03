26096ea95b15a3a3241f03438a931ee46eaeee8c
maximo95 <maximollallire@gmail.com>
2021-12-02 21:55:44 -0300

    desafio3

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar () {
+    return (
+        <nav>
+            <a  href= "#" className="nav-clase">home</a>
+            <a  href= "#" className="nav-clase">productos</a>
+            <a  href= "#" className="nav-clase">nosotros</a>
+            <a  href= "#" className="nav-clase">contacto</a>
+        </nav>
+    )
+}
+
+export default Navbar;
